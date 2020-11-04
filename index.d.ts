9e804ef78aa07052a703512859ba0445c934df09
Tom Xu <ycxzhkx@gmail.com>
2020-11-04 10:07:23 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -11,5 +11,25 @@ type Callback = (message: PushMessage) => void;
 export default class AliyunPuth {
   static addListener(callback: Callback): void;
   static removeListener(callback: Callback): void;
+  /**
+   * 同步角标数到阿里云服务端 (仅ios支持)
+   */
+  static syncBadgeNum(num: number): void;
+  /**
+   * 获取初始消息
+   */
+  static getInitialMessage(): Promise<PushMessage>;
+  /**
+   * 获取设备Id
+   */
   static getDeviceId(): Promise<string>;
+  /**
+   * 绑定账号
+   * @param account 账号
+   */
+  static bindAccount(account: string): Promise<string>;
+  /**
+   * 解绑账号
+   */
+  static unbindAccount(): Promise<string>;
 }
