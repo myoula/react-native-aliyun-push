6e377c9245c5e4812ce657c63338e1c0d68fded9
Tom Xu <ycxzhkx@gmail.com>
2021-01-03 16:58:40 +0800

    add comment

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,23 @@
 export interface PushMessage {
+  /**
+   * "notification":通知 或者 "message":消息
+   */
   type: 'notification' | 'message';
+  /**
+   * 推送通知/消息标题
+   */
   title: string;
+  /**
+   * 推送通知/消息具体内容
+   */
   body: string;
+  /**
+   * "opened":用户点击了通知, "removed"用户删除了通知, 其他非空值:用户点击了自定义action（仅限ios）
+   */
   actionIdentifier: string;
+  /**
+   * 用户附加的{key:value}的对象
+   */
   extras: Record<string, any>;
 }
 
