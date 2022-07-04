e7f27a2620b6ad0f2be48f43e7e686efc438022a
myoula <xfrog2007@gmail.com>
2022-07-04 21:03:49 +0800

    change method

--- android/src/main/java/org/wonday/aliyun/push/AliyunPushModule.java
+++ android/src/main/java/org/wonday/aliyun/push/AliyunPushModule.java
@@ -15,6 +15,7 @@ import android.content.BroadcastReceiver;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
